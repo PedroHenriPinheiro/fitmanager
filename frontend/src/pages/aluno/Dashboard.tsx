e168b79532ef2839306985da740a7dcde0743820
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2026-05-23 15:43:44 -0300

    Dashboard do aluno com front (v. 1)

--- frontend/src/pages/aluno/Dashboard.tsx
+++ frontend/src/pages/aluno/Dashboard.tsx
@@ -1,39 +1,62 @@
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import './AlunoDashboard.css'
 import treinos from './treinos';
 import iconePerfil from './iconePerfil.png';
 
+import barbell from './icons/barbell.svg';
+import time from './icons/time.svg';
+import user from './icons/user.svg';
+
 function AlunoDashboard() {
+     const navigate = useNavigate();
+
      return (
           <>
-               <div>
+               <div className='cabecalho'>
                     <div>
                          <h1>UNIFOR GYM</h1>
                          <p>Bem-vindo, aluno</p>
                     </div>
 
                     <div>
-                         <img src={iconePerfil} alt="Ícone do perfil" />
+                         <img src={user} className='user' alt="Ícone do perfil" />
                     </div>
                </div>
 
-               <div>
-                    <div>
+               <div className='principal'>
+                    <div className='acessoMeuTreino' onClick={() => navigate('/meu-treino')}>
+                         <img src={barbell} className='barbell' />
                          <h1>Meu treino</h1>
                          <p>Visualizar ficha de treinos</p>
 
-                         <div>
+                         <div className='treinoItens'>
                               {Object.entries(treinos).map(([key, treino]) => {
-                                   console.log(key)
-
                                    return (
-                                        <p key={key}>{treino.nome}</p>
+                                        <p className='treinoItem' key={key}>{treino.nome}</p>
                                    )
                               })}
                          </div>
                     </div>
 
-                    <div>
+                    <div className='acessoHorarios'>
+                         <img src={time} className='time'/>
+                         <h1>Horários</h1>
+                         <p>Visualizar ficha de treinos</p>
+                         <p>Seg. - Sex.: 6h às 22h</p>
+                    </div>
 
+                    <div className='acessoHistorico'>
+                         <h1>Histórico recente</h1>
+                              {Object.entries(treinos).map(([key, treino]) => {
+                                   return (
+                                        <div className='historicoItem'>
+                                             <p key={key}>{treino.nome}</p>
+                                             <p>05/05/2026</p>
+                                        </div>
+                                   )
+                              })}
                     </div>
                </div>
           </>
