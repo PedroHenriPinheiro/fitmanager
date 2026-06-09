eb38dc53c29863b4d06fb39f888366f8ae81f873
Pedro Pinheiro <pedrocaseis@hotmail.com>
2026-06-09 18:13:14 -0300

    Corrige import do DashboardGestor

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,9 +2,9 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import { PrivateRoute } from "../components/auth/PrivateRoutes";
 import { ROLES } from "../services/authService";
 import { LoginPage }          from "../pages/auth/Login";
-import { DashboardGestor }     from "../pages/gestor/dashboard";
-import { Dashboard } from "../pages/instrutor/dashboard";
-import { DashboardAluno }     from "../pages/aluno/dashboard";
+import  DashboardGestor  from "../pages/gestor/Dashboard";
+import { Dashboard } from "../pages/instrutor/Dashboard";
+import { DashboardAluno }     from "../pages/aluno/Dashboard";
 import { NotFound }           from "../pages/notFound/notFound";
 import { GerenciarTreino } from "../pages/instrutor/treinos/GerenciarTreino";
 
